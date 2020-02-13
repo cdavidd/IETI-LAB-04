dd256bd7ebd4defc253c3b36be477b54519cfc13
david <cdavidlopez@hotmail.com>
2020-02-12 23:46:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*import React from "react";
+import logo from "./logo.svg";
+import { Login } from "./components/Login";
+import Drawer from "./components/Drawer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drawer />
     </div>
   );
+}
+
+export default App;
+*/
+import React, { Component } from "react";
+import "./App.css";
+import { Login } from "./components/Login";
+import Home from "./components/Drawer";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const LoginView = () => <Login login={this.handleIsLoggedIn} />;
+    const TodoAppView = () => <Home />;
+    this.state = {
+      LoginView: LoginView,
+      TodoAppView: TodoAppView,
+      isLoggedIn: false
+    };
+    this.handleIsLoggedIn = this.handleIsLoggedIn.bind(this);
+  }
+
+  render() {
+    let redi = (
+      <Redirect
+        to={
+          this.state.isLoggedIn === false &&
+          localStorage.getItem("email") === null
+            ? "/"
+            : "/home"
+        }
+      />
+    );
+
+    return (
+      <Router>
+        <div className="App">
+          <div>
+            {redi}
+            <Route exact path="/" component={this.state.LoginView} />
+            <Route path="/home" component={this.state.TodoAppView} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+
+  handleIsLoggedIn() {
+    this.setState({
+      isLoggedIn: true
+    });
+  }
 }
 
 export default App;
